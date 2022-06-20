31dddb810a64ef527fe68e2a724c411d98651890
Emiliano Bazan-Zapata <emilianobz546@gmail.com>
2022-06-19 23:38:31 -0300

    Listado Categorias

---
+++ WebApi/WebApi/Controllers/CategoriaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Datos;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+
+        //accedemos al DbContext
+        public CategoriaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Categoria>? listaDeCategorias = _context.Categorias?.ToList();   
+
+
+            return View(listaDeCategorias);
+        }
+
+    }
+}
