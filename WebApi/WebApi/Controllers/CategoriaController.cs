5765cfed3fbfd3b99d61d97df46f8ea42c58b8af
Emiliano Bazan-Zapata <emilianobz546@gmail.com>
2022-06-20 00:09:01 -0300

    Insertando registros en masa

--- WebApi/WebApi/Controllers/CategoriaController.cs
+++ WebApi/WebApi/Controllers/CategoriaController.cs
@@ -16,29 +16,69 @@ namespace WebApi.Controllers
 
         public IActionResult Index()
         {
-            List<Categoria>? listaDeCategorias = _context.Categorias?.ToList();   
+            List<Categoria> listaDeCategorias = _context.Categorias?.ToList();
 
 
             return View(listaDeCategorias);
         }
 
         [HttpGet]
-        public IActionResult Crear() 
+        public IActionResult Crear()
         {
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Crear(Categoria categoria) 
+        public IActionResult Crear(Categoria categoria)
         {
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
                 _context.Categorias?.AddAsync(categoria);
-                _context.SaveChanges();
+                _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult CrearMultipleOpcion2()
+        {
+            List<Categoria> listaDeCategoria = new List<Categoria>();
+            for (int i = 0; i < 2; i++)
+            {
+                listaDeCategoria.Add(new Categoria
+                {
+                    Nombre = Guid.NewGuid().ToString()
+                });
+                //_context.Categorias.AddAsync(new Categoria 
+                //{ 
+                //    Nombre = Guid.NewGuid().ToString() 
+                //});
+            }
+            _context.AddRangeAsync(listaDeCategoria);
+            _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult CrearMultipleOpcion5()
+        {
+            List<Categoria> listaDeCategoria = new List<Categoria>();
+            for (int i = 0; i < 5; i++)
+            {
+                listaDeCategoria.Add(new Categoria
+                {
+                    Nombre = Guid.NewGuid().ToString()
+                });
+                //_context.Categorias.AddAsync(new Categoria 
+                //{ 
+                //    Nombre = Guid.NewGuid().ToString() 
+                //});
+            }
+            _context.AddRangeAsync(listaDeCategoria);
+            _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
