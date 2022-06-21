947a8569c580d4a136b876a7618f7235c5dbf8fc
Emiliano Bazan-Zapata <emilianobz546@gmail.com>
2022-06-21 14:02:47 -0300

    Leyedo los articulos

---
+++ WebApi/WebApi/Controllers/ArticuloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Datos;
+
+namespace WebApi.Controllers
+{
+    public class ArticuloController : Controller
+    {
+        private readonly ApplicationDbContext _contex;
+        public ArticuloController(ApplicationDbContext context)
+        {
+            _contex = context;
+        }
+        public IActionResult Index()
+        {
+            var articulos = _contex.Articulos.ToList();
+            return View(articulos);
+        }
+    }
+}
