ab164ac1ba667ddf3a9822e53c395139e6d739ae
Emiliano Bazan-Zapata <emilianobz546@gmail.com>
2022-06-26 20:54:41 -0300

    Leyendo Usuarios

---
+++ WebApi/WebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Datos;
+
+namespace WebApi.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly ApplicationDbContext _contex;
+        public UsuarioController(ApplicationDbContext context)
+        {
+            _contex = context;
+        }
+        public IActionResult Index()
+        {
+            var usuarios = _contex.Usuarios.ToList();
+            return View(usuarios);
+        }
+    }
+}
