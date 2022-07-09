0f83002610cd27e1cba4db482439445b9dc3a775
Emiliano Bazán-Zapata <ebazan@midasconsultores.com.ar>
2022-07-09 12:53:11 -0300

    Listado y Agregando Etiquetas

---
+++ WebApi/WebApi/Controllers/EtiquetaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Datos;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class EtiquetaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EtiquetaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        { 
+            var etiquetas = _context.Etiquetas.ToList();
+            return View(etiquetas);
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Etiqueta etiqueta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Etiquetas?.AddAsync(etiqueta);
+                _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+    }
+}
